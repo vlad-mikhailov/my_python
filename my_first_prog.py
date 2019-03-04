1a830fd8e03f47845a2a0f7ece640eb0e2cfb327
vlad-mikhailov <asonovlad@gmail.com>
2019-03-04 19:08:18 +0300

    init commit

---
+++ my_first_prog.py
@@ -0,0 +1,6 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:59:38) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello')
+Hello
+>>> 
